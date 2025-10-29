b995b312a4aa81f9f7fd75bc7edeb6ece28931ab
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-10-29 15:30:43 +0300

    create hex color class

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quetes/core/utils/app_colors.dart';
 import 'package:quetes/core/utils/app_strings.dart';
+import 'package:quetes/core/utils/hex_color.dart';
 import 'package:quetes/features/random_quote/presentation/screens/quote_screen.dart';
 
 class QuoteApp extends StatelessWidget {
@@ -12,7 +12,7 @@ class QuoteApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: AppStrings.appName,
       theme: ThemeData(
-       primaryColor: AppColors.primary,
+       primaryColor: HexColor('#451BAB'),
         useMaterial3: true,
       ),
       home: const QuoteScreen(),
