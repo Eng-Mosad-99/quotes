e479b826d33d0d2d49eb1681e2e9fdc5ce1c4853
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-11-17 13:29:22 +0200

    Implementation of Repository

--- lib/features/random_quote/data/repositories/quote_repo_impl.dart
+++ lib/features/random_quote/data/repositories/quote_repo_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:quetes/core/errors/exceptions.dart';
 import 'package:quetes/core/errors/failures.dart';
 import 'package:quetes/core/network/network_info.dart';
 import 'package:quetes/features/random_quote/data/datasources/random_quote_local_data_source.dart';
@@ -16,8 +17,21 @@ class QuoteRepositoryImpl implements QuoteRepository {
     required this.localDataSource,
   });
   @override
-  Future<Either<Failure, QuoteEntity>> getRandomQuote() {
-    // TODO: implement getRandomQuote
-    throw UnimplementedError();
+  Future<Either<Failure, QuoteEntity>> getRandomQuote() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteQuote = await remoteDataSource.getRandomQuote();
+        return Right(remoteQuote);
+      } on ServerException catch (e) {
+        return Left(ServerFailure(e.toString()));
+      }
+    } else {
+      try {
+        final localQuote = await localDataSource.getLastRandomQuote();
+        return Right(localQuote);
+      } on CacheException catch (e) {
+        return left(CacheFailure(e.toString()));
+      }
+    }
   }
 }
