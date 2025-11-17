d6a3f4503dc6175ad305cc87771f2d87c8abe458
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-11-17 14:15:36 +0200

    Remote Data Source Part1

---
+++ lib/core/api/end_point.dart
@@ -0,0 +1,4 @@
+class EndPoint {
+  static const String baseUrl = 'https://quotes.stormconsultancy,co.uk/';
+  static const String randomQuote = '${baseUrl}random.json';
+}
--- lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
+++ lib/features/random_quote/data/datasources/random_quote_remote_data_source.dart
@@ -1,5 +1,31 @@
+import 'dart:convert';
+
+import 'package:quetes/core/api/end_point.dart';
+import 'package:quetes/core/errors/exceptions.dart';
 import 'package:quetes/features/random_quote/data/models/quote_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class RandomQuoteRemoteDataSource {
   Future<QuoteModel> getRandomQuote();
+}
+
+class RandomQuoteRemoteDataSourceImpl implements RandomQuoteRemoteDataSource {
+  final http.Client client;
+
+  RandomQuoteRemoteDataSourceImpl({required this.client});
+  @override
+  Future<QuoteModel> getRandomQuote() async {
+    final randomQuoteUri = Uri.parse(EndPoint.randomQuote);
+    final response = await client.get(
+      randomQuoteUri,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    if (response.statusCode == 200) {
+      return QuoteModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
