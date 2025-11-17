2898ae20a46a621d32dcd60a6ec530d0d20f2278
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-11-17 12:57:27 +0200

    create general use case and use it.

--- lib/features/random_quote/domain/usecases/get_random_quote_use_case.dart
+++ lib/features/random_quote/domain/usecases/get_random_quote_use_case.dart
@@ -1,9 +1,31 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:quetes/core/errors/failures.dart';
+import 'package:quetes/core/use_case/use_case.dart';
 import 'package:quetes/features/random_quote/domain/entities/quote_entity.dart';
 
-class GetRandomQuoteUseCase {
-  Future<Either<Failure, QuoteEntity>>? execute() {
-    return null;
+class GetRandomQuoteUseCase implements UseCase<QuoteEntity, NoParams> {
+  final QuoteRepository repository;
+
+  GetRandomQuoteUseCase(this.repository);
+  @override
+  Future<Either<Failure, QuoteEntity>> call(NoParams params) {
+    return repository.getRandomQuote();
   }
 }
+
+//! if params
+// class LoginParams extends Equatable {
+//   final String userName;
+//   final String password;
+
+//   const LoginParams({
+//     required this.userName,
+//     required this.password,
+//   });
+//   @override
+//   List<Object?> get props => [
+//         userName,
+//         password,
+//       ];
+// }
